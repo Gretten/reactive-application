9321471c712d1ec52744427be95a63229217eaf1
Gretten <shhworkmail@yandex.ru>
2019-01-21 01:23:46 +0300

    stage 37 complited

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+import './todo-list-item.css';
+
+export default class TodoListItem extends Component { 
+
+  // Можно инициализировать state с помощью полей класса, не используя конструктор
+  state = {
+    done: false
+  }
+
+  onLabelClick = () => {
+    this.setState({
+      done: true,
+      important: false
+    });
+  };
+
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    })
+  }
+
+  render() {
+
+    const { label } = this.props;
+    const { done, important } = this.state;
+    let classNames = 'todo-list-item';
+
+    if(done) {
+      classNames += ' done';
+    }
+    if(important) {
+      classNames += ' important'
+    }
+  
+    return (
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          onClick = { this.onLabelClick }
+          >
+          {label}
+        </span>
+  
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImportant}
+                >
+          <i className="fa fa-exclamation" />
+        </button>
+  
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  };
+}
